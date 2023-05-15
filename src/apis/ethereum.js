323ed660661211f379400148add9714bf957de09
Harshit <7harshit20@gmail.com>
2023-05-15 13:31:56 +0530

    added last block and last tx tables

--- src/apis/ethereum.js
+++ src/apis/ethereum.js
@@ -1,44 +1,47 @@
-import React from "react";
 import axios from "axios";
 
-  export const searchTransactions = async (th) => {
-    const response = await axios.get(
-      `https://api.blockchair.com/ethereum/dashboards/transaction/${th}`
-    );
-    console.log(response.data.result);
-    return response.data.result;
-  };
+export const searchTransactions = async (th) => {
+  const response = await axios.get(
+    `https://api.blockchair.com/ethereum/dashboards/transaction/${th}`
+  );
+  console.log(response.data.result);
+  return response.data.result;
+};
 
-  export const searchBlocks = async (bl) => {
-    const response = await axios.get(
-      `https://api.blockchair.com/ethereum/dashboards/block/${bl}`
-    );
-    console.log(response.data.result);
-    return response.data.result;
-  };
+export const searchBlocks = async (bl) => {
+  const response = await axios.get(
+    `https://api.blockchair.com/ethereum/dashboards/block/${bl}`
+  );
+  console.log(response.data.result);
+  return response.data.result;
+};
 
-  export const searchAddress = async (ad) => {
-    const response = await axios.get(
-      `https://api.blockchair.com/ethereum/dashboards/address/${ad}`
-    );
-    console.log(response.data.result);
-    return response.data.result;
-  };
+export const searchAddress = async (ad) => {
+  const response = await axios.get(
+    `https://api.blockchair.com/ethereum/dashboards/address/${ad}`
+  );
+  console.log(response.data.result);
+  return response.data.result;
+};
 
-  export const searchBlockHeight = async () => {
-    const response = await axios.get(
-      `https://api.blockchair.com/ethereum/stats`
-    );
-    console.log(response.data.data.blocks);
-    return response.data.data.blocks;
-  };
+export const searchBlockHeight = async () => {
+  const response = await axios.get(`https://api.blockchair.com/ethereum/stats`);
+  console.log(response.data.data.blocks);
+  return response.data.data.blocks;
+};
 
-  export const getLastTenBlocks = async () => {
-    const response = await axios.get(
-      `https://api.blockchair.com/ethereum/blocks?limit=10`
-    );
-    console.log(response.data.data);
-    return response.data.data;
-  };
+export const getLastTenBlocks = async () => {
+  const response = await axios.get(
+    `https://api.blockchair.com/ethereum/blocks?limit=10`
+  );
+  console.log(response.data.data);
+  return response.data.data;
+};
 
-
+export const getLastTenTransactions = async () => {
+  const response = await axios.get(
+    `https://api.blockchair.com/ethereum/transactions?limit=10`
+  );
+  console.log(response.data.data);
+  return response.data.data;
+};
--- src/components/pages/Ethereum.js
+++ src/components/pages/Ethereum.js
@@ -1,19 +1,78 @@
-import React, { useState,useEffect } from "react";
-import {getLastTenBlocks} from '../../apis/ethereum';
+import React, { useState, useEffect } from "react";
+import {
+  getLastTenBlocks,
+  searchBlockHeight,
+  getLastTenTransactions,
+} from "../../apis/ethereum";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Ethereum = () => {
   const [search, setSearch] = useState("");
+  const [blockHeight, setBlockHeight] = useState("");
+  const [lastTenBlocks, setLastTenBlocks] = useState([]);
+  const [lastTenTransactions, setLastTenTransactions] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [txrows, setTxRows] = useState([]);
 
-  useEffect(()=>{
-    async function fetchTenBlocks()
-    {
-      const res=await getLastTenBlocks();
-      console.log(res);
-    }
-   
-    fetchTenBlocks()
+  function createData(block, miner, transactions, reward, time) {
+    return { block, miner, transactions, reward, time };
   }
-  ,[])
+  function createTxData(txhash, sender, recipient, value, time) {
+    return { txhash, sender, recipient, value, time };
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const blockres = await getLastTenBlocks();
+      setLastTenBlocks(blockres);
+      // console.log(blockres);
+
+      const sizeres = await searchBlockHeight();
+      setBlockHeight(sizeres);
+
+      const txres = await getLastTenTransactions();
+      setLastTenTransactions(txres);
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setRows(
+      lastTenBlocks.map((block) => {
+        return createData(
+          block.id,
+          block.miner,
+          block.transaction_count,
+          block.reward,
+          block.time
+        );
+      })
+    );
+  }, [lastTenBlocks]);
+
+  useEffect(() => {
+    setTxRows(
+      lastTenTransactions.map((tx) => {
+        return createTxData(
+          tx.hash,
+          tx.sender,
+          tx.recipient,
+          tx.value,
+          tx.time
+        );
+      })
+    );
+    console.log("txrows", txrows);
+  }, [lastTenTransactions]);
+
   return (
     <div>
       <div class='row'>
@@ -27,7 +86,7 @@ const Ethereum = () => {
               type='search'
               placeholder='Search by transaction hash, block hash, address, or block height'
               id='example-search-input'
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => setSearch(e.target.reward)}
             />
             <span class='input-group-append'>
               <button
@@ -39,9 +98,75 @@ const Ethereum = () => {
             </span>
           </div>
         </div>
-        
       </div>
-       
+      <div class='row'>
+        <div class='col-md-10 mx-auto'>
+          <div class='large mt-5 mb-2 fw-light'>Latest Blocks</div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Blocks</TableCell>
+                  <TableCell align='left'>miner</TableCell>
+                  <TableCell align='left'>transactions</TableCell>
+                  <TableCell align='left'>reward&nbsp;(eth)</TableCell>
+                  <TableCell align='left'>Time</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.block}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component='th' scope='row'>
+                      {row.block}
+                    </TableCell>
+                    <TableCell align='left'>{row.miner}</TableCell>
+                    <TableCell align='left'>{row.transactions}</TableCell>
+                    <TableCell align='left'>{row.reward / 1e18}</TableCell>
+                    <TableCell align='left'>{row.time}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='col-md-10 mx-auto'>
+          <div class='large mt-5 mb-2 fw-light'>Latest Transactions</div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Tx Hash</TableCell>
+                  <TableCell align='left'>Sender</TableCell>
+                  <TableCell align='left'>Recipient</TableCell>
+                  <TableCell align='left'>Value&nbsp;(eth)</TableCell>
+                  <TableCell align='left'>Time</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {txrows.map((row) => (
+                  <TableRow
+                    key={row.block_id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component='th' scope='row'>
+                      {row.txhash ? row.txhash.substring(0, 20) + "..." : null}
+                    </TableCell>
+                    <TableCell align='left'>{row.sender}</TableCell>
+                    <TableCell align='left'>{row.recipient}</TableCell>
+                    <TableCell align='left'>{row.value / 1e18}</TableCell>
+                    <TableCell align='left'>{row.time}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
     </div>
   );
 };
