fb3bb26a53b63d4ceba6af743c2c6d64c1d80a38
Harshit <7harshit20@gmail.com>
2023-05-18 13:26:21 +0530

    added block table and address txs table

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Address from "./components/layout/Ethereum/Address";
 import Transaction from "./components/layout/Ethereum/Transaction";
 
 import Bitcoin from "./components/pages/Bitcoin";
-import BlockDetailsBitcoin from "./components/layout/BlockDetailsBitcoin";
-import TransactionDetailsBitcoin from "./components/layout/TransactionDetailsBitcoin";
+import BlockDetailsBitcoin from "./components/layout/Bitcoin/BlockDetailsBitcoin";
+import TransactionDetailsBitcoin from "./components/layout/Bitcoin/TransactionDetailsBitcoin";
 import AddressDetailsBitcoin from "./components/layout/Bitcoin/AddressDetailsBitcoin";
 import Block from "./components/layout/Ethereum/Block";
 import Transactions from "./components/layout/Ethereum/Transactions";
+import Blocks from "./components/layout/Ethereum/Blocks";
 // import Search from "./components/layout/Search";
 
 // import TextField from "@mui/material/TextField";
@@ -28,14 +29,25 @@ const App = () => {
             <Route path='' element={<Transactions />} />
             <Route path=':txhash' element={<Transaction />} />
           </Route>
-          {/* <Route path='transaction/:txhash' element={<Transaction />} /> */}
-          <Route path='block/:blockId' element={<Block />} />
+          <Route path='block'>
+            <Route path='' element={<Blocks />} />
+            <Route path=':blockId' element={<Block />} />
+          </Route>
           <Route path='address/:address' element={<Address />} />
         </Route>
         <Route path='/bitcoin' element={<Bitcoin />} />
-        <Route path='/bitcoin/block/:blockId' element={<BlockDetailsBitcoin />} />
-        <Route path='/bitcoin/transaction/:transactionId' element={<TransactionDetailsBitcoin />}  />
-        <Route path='/bitcoin/address/:address' element={<AddressDetailsBitcoin />}  />
+        <Route
+          path='/bitcoin/block/:blockId'
+          element={<BlockDetailsBitcoin />}
+        />
+        <Route
+          path='/bitcoin/transaction/:transactionId'
+          element={<TransactionDetailsBitcoin />}
+        />
+        <Route
+          path='/bitcoin/address/:address'
+          element={<AddressDetailsBitcoin />}
+        />
         {/* <Route path='/bitcoin' element={<Ethereum />} /> */}
       </Routes>
     </Router>
